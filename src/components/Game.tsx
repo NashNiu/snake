b3ac05d44a278273ab16b7188d265a4b06153a57
niutengfei <niutengfei@magiccompass.cn>
2026-05-11 19:06:29 +0800

    feat: add canvas with bloom post-processing

---
+++ src/components/Game.tsx
@@ -0,0 +1,22 @@
+// src/components/Game.tsx
+import { Canvas } from '@react-three/fiber'
+import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { GameScene } from './GameScene'
+
+export function Game() {
+  return (
+    <Canvas
+      camera={{ position: [0, 4, 16], fov: 75, near: 0.1, far: 500 }}
+      style={{ width: '100vw', height: '100vh' }}
+    >
+      <GameScene />
+      <EffectComposer>
+        <Bloom
+          luminanceThreshold={0.2}
+          luminanceSmoothing={0.9}
+          intensity={1.5}
+        />
+      </EffectComposer>
+    </Canvas>
+  )
+}
